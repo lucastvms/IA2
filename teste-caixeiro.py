dc1bd46d64abc84462a0e997857ad6d58371bc0b
Lucas Vieira <ltv@ic.ufal.br>
2019-01-26 10:17:14 -0200

    Geração funcionando. Adaptação calculando o fitness. Falta ordenar e criar a população intermediária para Mutação.

--- teste-caixeiro.py
+++ teste-caixeiro.py
@@ -1,33 +1,38 @@
-# -*- coding: utf-8 -*-
 import random
+from operator import itemgetter
 
 # http://www.dainf.ct.utfpr.edu.br/~tacla/IA/016a-AlgGeneticos.pdf
 # https://pythonhelp.wordpress.com/2012/06/15/por-que-__name__-__main__/
 
-#1. Geração de população
-#Matriz com os cromossomos sendo representados por listas, gerados de forma aleatória
+# 1. Geração de população
+# Matriz com os cromossomos sendo representados por listas, gerados de forma aleatória
 def gerar(n_linhas, n_colunas):
-    matriz = [] # Matriz
-    linha = [] # Linha
+    pop = [] # População
+    linha = [] # Cromossomo
 
-    while n_linhas != len(matriz): # Quando o número de elementos da matriz(linhas) forem diferentes da quantidade máxima definida pelo usuário, ele ficará rodando.
-        n = random.randint(0, 1) # Utilizei random para adicionar os valores
-        linha.append(n) # Adiciono n à linha
+    # gera cromossomos aleatórios e binários
+    while n_linhas != len(pop):
+        n = random.randint(0, 1)
+        linha.append(n)
 
-        if len(linha) == n_colunas: # Se a quantidade de elementos for igual à quantidade de colunas definida pelo usuário :
-            matriz.append(linha) # Adiciono a linha à matriz
-            linha = [] # E zero a "linha" para adicionar outra à matriz
-
-    return matriz # Retorno a mesma
+        if len(linha) == n_colunas:
+            pop.append(linha)
+            linha = []
 
 
-#2. Adaptação
-# avaliar o peso dos cromossomos, para saber o ranking deles
-def adaptar(pop, pesos ,origem, destino):
+    print(pop, "\n")
+
+    return pop
+
+
+# 2. Adaptação
+# Avaliar o peso dos cromossomos, para saber o ranking deles
+def adaptar(pop, pesos, origem, destino):
     fitness = [] # lista do fitness/ranking dos cromossomos
 
-    for i in range(0,10):
-        peso = 0 # peso do cromossomo
+    # cria fitness
+    for i in range(0, 10):
+        peso = 0
         lista_caminho = [] # guarda as origens e os destinos
 
         for j in range(0, 12):
@@ -35,11 +40,21 @@ def adaptar(pop, pesos ,origem, destino):
                 peso += pesos[j]
                 lista_caminho.append(origem[j])
                 lista_caminho.append(destino[j])
-                if(lista_caminho.count(origem[j]) > 2 or lista_caminho.count(origem[j]) > 2):
-                    peso = 10000;
-                    break;
+                if lista_caminho.count(origem[j]) > 2 or lista_caminho.count(origem[j]) > 2:
+                    peso = 10000
 
         fitness.append(peso)
+
+    print(fitness, "\n")
+    fitness.sort()
+    pop[:].extend(fitness)
+    pop = sorted(pop, key=itemgetter(len(pop)-1))
+
+    print(pop)
+
+    #aux = zip(fitness,  pop for i in range(0, 10))
+    #print (aux)
+    #fitness, pop[]= zip(*sorted(zip(fitness, pop[]))
 
     return fitness
 
@@ -58,9 +73,9 @@ pesos = [1, 5, 4, 1, 2, 6, 5, 2, 3, 4, 5, 3]
 origem = ['a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c', 'd', 'd', 'd']
 destino = ['b', 'c', 'd', 'a', 'c', 'd', 'a', 'b', 'd', 'a', 'b', 'c']
 pop = gerar(10, 12)
-print (pop)
 print (adaptar(pop, pesos, origem, destino))
 #201. Testes
+
 
 #print m[0][1]
 #for i in range (0, 10):    print i
